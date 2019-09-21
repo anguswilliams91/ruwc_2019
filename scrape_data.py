a769d0079921fd996d73987fd81e4a15a0f26d24
Angus Williams <anguswilliams91@gmail.com>
2019-09-21 13:28:16 +0100

    also scrape rankings data

--- scrape_data.py
+++ scrape_data.py
@@ -1,6 +1,7 @@
 """Scrape all men's international rugby match results from 1st January 2013 to present."""
 from bs4 import BeautifulSoup
 import urllib.request
+import json
 import re
 import pandas as pd
 
@@ -39,7 +40,7 @@ def scrape_page(url):
 
 def get_next_url(url, page_number):
     # get the next url to scrape in the set of results pages
-    soup = BeautifulSoup(urllib.request.urlopen(url))
+    soup = BeautifulSoup(urllib.request.urlopen(url), "html.parser")
     try:
         partial_url = soup.find_all(
             "a", href=True, attrs={"title": re.compile(f"go to page {page_number + 1}")}
@@ -51,7 +52,7 @@ def get_next_url(url, page_number):
 
 def scrape_all_pages(initial_page):
     # scrape all of the data
-    url = init_url
+    url = initial_page
     data = []
     i = 1
     while url is not None:
@@ -59,6 +60,25 @@ def scrape_all_pages(initial_page):
         url = get_next_url(url, i)
         i += 1
     return pd.concat(data)
+
+
+def make_ranking_url(date):
+    # return the url to query to obtain the world rankings on a given date
+    return f"https://cmsapi.pulselive.com/rugby/rankings/mru?date={date}&client=pulse"
+
+
+def make_ranking_dict(date):
+    # return the world rankings on the given date as a dictionary
+    url = make_ranking_url(date)
+    rankings = json.loads(urllib.request.urlopen(url).read())["entries"]
+    ranks = {team["team"]["name"]: i + 1 for i, team in enumerate(rankings)}
+    return ranks
+
+
+def make_rankings_table(dates):
+    # make a table of world rankings on the provided dates
+    ranking_dicts = {d: make_ranking_dict(str(d.date())) for d in dates}
+    return pd.DataFrame(ranking_dicts).T
 
 
 def main():
@@ -74,6 +94,10 @@ def main():
         (rugby_data["home_team"] != "") & (rugby_data["away_team"] != "")
     ]
     rugby_data[::2].to_csv("rugby_data.csv", index=False)
+
+    # scrape rankings data
+    rankings_table = make_rankings_table(pd.DatetimeIndex(rugby_data["date"].unique()))
+    rankings_table.to_csv("rankings_data.csv")
 
 
 if __name__ == "__main__":
